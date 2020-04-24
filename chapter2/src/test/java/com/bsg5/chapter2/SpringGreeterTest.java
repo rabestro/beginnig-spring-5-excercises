50c63316fc3ed2f148149785e70ad03468dd6e18
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-04-24 14:10:56 +0300

    Reformat code for tests

--- chapter2/src/test/java/com/bsg5/chapter2/SpringGreeterTest.java
+++ chapter2/src/test/java/com/bsg5/chapter2/SpringGreeterTest.java
@@ -11,14 +11,12 @@ import static org.testng.Assert.assertEquals;
 public class SpringGreeterTest {
     @Test
     public void testHelloWorld() {
+        final var context = new ClassPathXmlApplicationContext("/applicationContext.xml");
+        final var greeter = context.getBean("helloGreeter", Greeter.class);
 
-        var context = new ClassPathXmlApplicationContext("/applicationContext.xml");
-
-        var greeter = context.getBean("helloGreeter", Greeter.class);
-
-        var baos = context.getBean("baos", ByteArrayOutputStream.class);
+        final var baos = context.getBean("baos", ByteArrayOutputStream.class);
         greeter.greet();
-        var data = new String(baos.toByteArray(), StandardCharsets.UTF_8);
+        final var data = new String(baos.toByteArray(), StandardCharsets.UTF_8);
 
         assertEquals(data, "Hello, World!");
     }
