facfe31a4e7cdfc6807808162743e32ff2b13f7f
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-04-28 21:22:48 +0300

    Added test class TestLifeCycle06, TestLifeCycle07

---
+++ chapter4/src/test/java/com/bsg5/chapter4/TestLifeCycle06.java
@@ -0,0 +1,43 @@
+package com.bsg5.chapter4;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.stereotype.Component;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.testng.annotations.Test;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import static org.testng.Assert.*;
+
+@Component
+class SeventhObject extends HasData {
+    static Object semaphore = null;
+
+    @PostConstruct
+    public void initialize() throws Exception {
+        semaphore = new Object();
+    }
+
+    @PreDestroy
+    public void dispose() throws Exception {
+        semaphore = null;
+    }
+}
+
+@ContextConfiguration("/annotated-06.xml")
+public class TestLifeCycle06 extends AbstractTestNGSpringContextTests {
+    @Autowired
+    ConfigurableApplicationContext context;
+
+    @Test
+    public void testInitDestroyMethods() {
+        SeventhObject o1 = context.getBean(SeventhObject.class);
+        assertNotNull(SeventhObject.semaphore);
+        assertEquals(o1.getDatum(), "default");
+        context.close();
+        assertNull(SeventhObject.semaphore);
+    }
+}
