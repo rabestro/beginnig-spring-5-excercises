55fc38bd309a341afb3da9ba412155c64206d32b
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-05-03 19:57:04 +0300

    Added test class TestGreetingController

--- chapter7/src/main/java/com/bsg5/chapter7/Greeting.java
+++ chapter7/src/main/java/com/bsg5/chapter7/Greeting.java
@@ -1,19 +1,25 @@
 package com.bsg5.chapter7;
 
 import java.util.Objects;
+
 public class Greeting {
     String message;
+
     public Greeting(String message) {
         this.message = message;
     }
+
     public Greeting() {
     }
+
     public String getMessage() {
         return message;
     }
+
     public void setMessage(String message) {
         this.message = message;
     }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -21,6 +27,7 @@ public class Greeting {
         Greeting greeting = (Greeting) o;
         return Objects.equals(getMessage(), greeting.getMessage());
     }
+
     @Override
     public int hashCode() {
         return Objects.hash(getMessage());
